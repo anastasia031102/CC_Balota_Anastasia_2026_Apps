49479b181811cd8eb86f6e44599b40e33da0132d
anastasia031102 <126573013+anastasia031102@users.noreply.github.com>
2026-06-04 22:12:44 +0300

    Fix: final domain and client ID configuration

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -4,9 +4,9 @@ export const LOGOUT_URI = "https://app-tucn-cc-dev-balotaa.azurewebsites.net/";
 
 export const OIDC_CONFIG = {
   authority: "https://cognito-idp.eu-north-1.amazonaws.com/eu-north-1_jyLIY5S7E",
-  client_id: "66qeenpqcd42fgcubpeunepbr0", // ID-ul tău real din AWS
+  client_id: "66qeenpqcd42fgcubpeunepbr0",
   redirect_uri: "https://app-tucn-cc-dev-balotaa.azurewebsites.net/",
-  response_type: "code", // ⬅️ Rutarea originală pe bază de Code pe care o vrea profesorul!
+  response_type: "code",
   scope: "openid email profile",
-  loadUserInfo: false
+  loadUserInfo: false,
 };
